f2d1723bb540392e35878f22cf2da5cdb8b0bfb7
Jyoti Hambir <jyotiqa2022@gmail.com>
2025-04-22 14:51:36 +0100

    updated urls and views of customer app

--- annapurna/annapurna/settings.py
+++ annapurna/annapurna/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'annapurna.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'customer' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
