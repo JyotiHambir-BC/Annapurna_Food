95b554eae89640a1564ecc7bd6c17cceff48a881
Jyoti Hambir <jyotiqa2022@gmail.com>
2025-04-24 14:43:35 +0100

    add models for recipes

--- annapurna/annapurna/urls.py
+++ annapurna/annapurna/urls.py
@@ -2,11 +2,13 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from customer.views import Index, About
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', Index.as_view(), name='index'),
     path('about/', About.as_view(), name='about'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- annapurna/customer/models.py
+++ annapurna/customer/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class RecipeItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='recipe_images/')
+    required_time = models.DecimalField(max_digits=5, decimal_places=2)
+    category = models.ManyToManyField('category', related_name='recipe')
+
+    def __str__(self):
+        return self.name
+    
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+
