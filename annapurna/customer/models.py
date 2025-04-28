050bfb96995269077cd665e35d80599adbca9269
Jyoti Hambir <jyotiqa2022@gmail.com>
2025-04-28 11:07:57 +0100

    create views for Recipe in customer/views.py

--- annapurna/customer/models.py
+++ annapurna/customer/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 class RecipeItem(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
+    name = models.CharField(max_length=100)    
     image = models.ImageField(upload_to='recipe_images/')
+    ingredients = models.TextField()
+    how_to_make = models.TextField()
     required_time = models.DecimalField(max_digits=5, decimal_places=2)
     category = models.ManyToManyField('category', related_name='recipe')
 
--- annapurna/customer/views.py
+++ annapurna/customer/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from .models import RecipeItem, Category
 
 class Index(View):
     def get(self, request, *args, **kwargs):
@@ -11,9 +12,25 @@ class About(View):
     
 class Recipe(View):
     def get(self, request, *args, **kwargs):
-        pass
         # get every item from each category
+        appetizers = RecipeItem.object.filter(categoty__name__contains = 'Appetizer')
+        rice_dishes = RecipeItem.object.filter(categoty__name__contains = 'Rice Dishes')
+        chuttney = RecipeItem.object.filter(categoty__name__contains = 'Chuttney')
+        drinks = RecipeItem.object.filter(categoty__name__contains = 'Drinks')
+        curry = RecipeItem.object.filter(categoty__name__contains = 'Curry')
+        dessert = RecipeItem.object.filter(categoty__name__contains = 'Dessert')
+        snacks = RecipeItem.object.filter(categoty__name__contains = 'Snacks')
         
         # pass into context
+        context = {
+            'appetizers': appetizers,
+            'rice_dishes': rice_dishes,
+            'chuttney': chuttney,
+            'drinks': drinks,
+            'curry': curry,
+            'dessert': dessert,
+            'snacks': snacks,
+        }
 
         # render the template
+        return render(request, 'customer/recipe.html')
