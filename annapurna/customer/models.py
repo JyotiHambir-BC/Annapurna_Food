aaff646d6bc7dc06736d1d2457550bd9d7221d69
Jyoti Hambir <jyotiqa2022@gmail.com>
2025-04-30 14:54:47 +0100

    added image for appetizers

--- annapurna/customer/models.py
+++ annapurna/customer/models.py
@@ -1,16 +1,6 @@
 from django.db import models
 from django.utils.text import slugify
 
-class RecipeItem(models.Model):
-    name = models.CharField(max_length=100)    
-    image = models.ImageField(upload_to='recipe_images/')
-    ingredients = models.TextField()
-    how_to_make = models.TextField()
-    required_time = models.DecimalField(max_digits=5, decimal_places=2)
-    category = models.ManyToManyField('category', related_name='recipe')
-
-    def __str__(self):
-        return self.name
     
 class Category(models.Model):
     image = models.ImageField(upload_to='category_images/', null=True, blank=True)
@@ -21,6 +11,17 @@ class Category(models.Model):
         if not self.slug:
             self.slug = slugify(self.name)
         super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+    
+class RecipeItem(models.Model):
+    name = models.CharField(max_length=100)    
+    image = models.ImageField(upload_to='recipe_images/')
+    ingredients = models.TextField()
+    how_to_make = models.TextField()
+    required_time = models.DecimalField(max_digits=5, decimal_places=2)
+    category = models.ForeignKey(Category, related_name='recipe', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
--- annapurna/customer/views.py
+++ annapurna/customer/views.py
@@ -9,52 +9,45 @@ class Index(View):
 class About(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'customer/about.html')
-    
-# class Recipe(View):
-#     def get(self, request, *args, **kwargs):
-#         # get every item from each category
-#         appetizers = RecipeItem.objects.filter(category__name__contains = 'Appetizer')
-#         rice_dishes = RecipeItem.objects.filter(category__name__contains = 'Rice Dishes')
-#         chuttney = RecipeItem.objects.filter(category__name__contains = 'Chuttney')
-#         drinks = RecipeItem.objects.filter(category__name__contains = 'Drinks')
-#         curry = RecipeItem.objects.filter(category__name__contains = 'Curry')
-#         dessert = RecipeItem.objects.filter(category__name__contains = 'Dessert')
-#         snacks = RecipeItem.objects.filter(category__name__contains = 'Snacks')
-        
-#         # pass into context
-#         context = {
-#             'appetizers': appetizers,
-#             'rice_dishes': rice_dishes,
-#             'chuttney': chuttney,
-#             'drinks': drinks,
-#             'curry': curry,
-#             'dessert': dessert,
-#             'snacks': snacks,
-#         }
-
-#         # render the template
-#         return render(request, 'customer/recipe.html', context)
-    
-#     def post(self, request, *args, **kwargs):
-#         return render(request, 'customer/recipe.html')
-    
-# class Detail_Recipe(View):
-#     def get(self, request, *args, **kwargs):
-#         return render(request, 'customer/detail_recipe.html')
 
 class RecipeCategoryView(View):
     def get (self, request, *args, **kwargs):
         categories = Category.objects.all()
-        return render(request, 'customer/recipe_category.html', {'categories': categories})    
-
-    
-    
+        return render(request, 'customer/recipe_category.html', {'categories': categories})
     
 class RecipeListView(View):
-    def get (self, request, category_slug):
+    def get(self, request, category_slug):
+        # Get the category based on the slug
         category = get_object_or_404(Category, slug=category_slug)
+
+         # Filter RecipeItems based on the selected category
         recipes = RecipeItem.objects.filter(category=category)
-        return render(request, 'customer/recipe_list.html', {
-            'category' : category,
-            'recipes' : recipes
-        })
+
+        context = {
+            'category': category,
+            'recipes': recipes,
+        }
+
+        
+       # Filter the RecipeItems by the category
+        # appetizers = RecipeItem.objects.filter(category=category)
+        # snacks = RecipeItem.objects.filter(category=category)
+        # desserts = RecipeItem.objects.filter(category=category)
+        # chuttney = RecipeItem.objects.filter(category=category)
+        # curry = RecipeItem.objects.filter(category=category)
+        # drinks = RecipeItem.objects.filter(category=category)
+        # rice_dishes = RecipeItem.objects.filter(category=category)
+
+         # Add everything to the context
+        # context = {
+        #     'category': category,
+        #     'appetizers': appetizers,
+        #     'snacks': snacks,
+        #     'desserts': desserts,
+        #     'chuttney': chuttney,
+        #     'curry': curry,
+        #     'drinks': drinks,
+        #     'rice_dishes': rice_dishes,
+        # }
+
+        return render(request, 'customer/recipe_list.html', context)
